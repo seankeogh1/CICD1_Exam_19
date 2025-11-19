46fca2a9161e841d4fde621399ffeb23c166dfca
SEANKEOGH-STUDENT <G00432475@atu.ie>
2025-11-19 14:03:48 +0000

    initial

---
+++ src/test/java/ie/atu/cicd1_exam_19/Cicd1Exam19ApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.cicd1_exam_19;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cicd1Exam19ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
